0feca284004de6d0887d8084ba26d4577d24fb6e
sadova-nadezhda <109034129+sadova-nadezhda@users.noreply.github.com>
2023-10-10 21:28:09 +0600

    update project v23.5

--- project1/mysite/index/forms.py
+++ project1/mysite/index/forms.py
@@ -1,9 +1,11 @@
 from  django import forms
 from .models import *
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form__input'}))
-    slug = forms.SlugField(max_length=255, label='URL')
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows':10}), label='Контент', required=False)
-    is_published = forms.BooleanField(label='Публикация', initial=True)
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категории', empty_label='Выберите категорию')
+class AddPostForm(forms.ModelForm):
+    class Meta:
+        model = Info
+        fields = ['title', 'slug', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form__input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows':10}),
+        }
