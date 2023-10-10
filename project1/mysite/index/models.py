843802c515daf443f17c67ab7d8b31b2ba59a54f
sadova-nadezhda <109034129+sadova-nadezhda@users.noreply.github.com>
2023-10-10 19:31:22 +0600

    update project v23

--- project1/mysite/index/models.py
+++ project1/mysite/index/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 class Info(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True, verbose_name='Описание')
     photo = models.ImageField(upload_to='photoes/%Y/%m/%d/', verbose_name='Фото', null=True)
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
@@ -23,6 +24,7 @@ class Info(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     def __str__(self):
         return self.name
     def get_absolute_url(self):
